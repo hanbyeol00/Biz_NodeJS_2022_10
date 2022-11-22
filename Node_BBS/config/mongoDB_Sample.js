6cde2e600d28594cee95f8e90f20cbc3d849ed30
hanbyeol00 <gksquf6670@gmail.com>
2022-11-22 09:49:25 +0900

    mongoDB_procet

---
+++ Node_BBS/config/mongoDB_Sample.js
@@ -0,0 +1,1 @@
+const atliasURL = "YOUR MONGO ATLAS URL";
---
+++ Node_ERP_V2 - 복사본/public/js/users/bltBrd.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const aNotice = document.querySelector("a.Notice");
+  const aAll = document.querySelector("a.all");
+  const aConcert_review = document.querySelector("a.concert_review");
+  const aConcert_hall_review = document.querySelector("a.concert_hall_review");
+  const aFreeboard = document.querySelector("a.freeboard");
+  if (category == "all") {
+    aAll.style.backgroundColor = "gray";
+    aAll.style.color = "white";
+  }
+  if (category == "notice") {
+    aNotice.style.backgroundColor = "gray";
+    aNotice.style.color = "white";
+  }
+  if (category == "concert_review") {
+    aConcert_review.style.backgroundColor = "gray";
+    aConcert_review.style.color = "white";
+  }
+  if (category == "concert_hall_review") {
+    aConcert_hall_review.style.backgroundColor = "gray";
+    aConcert_hall_review.style.color = "white";
+  }
+  if (category == "freeboard") {
+    aFreeboard.style.backgroundColor = "gray";
+    aFreeboard.style.color = "white";
+  }
+});
