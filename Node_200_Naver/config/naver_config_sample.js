6995dd5dec6b4339aa7f3aa852af95fa2a4daf19
hanbyeol00 <gksquf6670@gmail.com>
2023-01-13 09:26:56 +0900

    naver

--- Node_200_Naver/config/naver_config_sample.js
+++ Node_200_Naver/config/naver_config_sample.js
@@ -1,9 +1,9 @@
 // X-Naver-Client-Id: {애플리케이션 등록 시 발급받은 클라이언트 아이디 값}
 // X-Naver-Client-Secret: {애플리케이션 등록 시 발급받은 클라이언트 시크릿 값}
-const CLIENT_ID = { KEY: "X-Naver-Client-Id", VALUE: "0SEzfbdFf7ZeWLetwSn6" };
+const CLIENT_ID = { KEY: "X-Naver-Client-Id", VALUE: "YOUR CLIENT ID" };
 const CLIENT_SECRET = {
   KEY: "X-Naver-Client-Secret",
-  VALUE: "CWljdk93N3",
+  VALUE: "YOUR CLIENT SECRET",
 };
 const BOOK_URL_JSON = "https://openapi.naver.com/v1/search/book.json";
 
