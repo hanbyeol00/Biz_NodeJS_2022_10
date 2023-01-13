533502a6b270912deefca6f0f4da37d07853d928
hanbyeol00 <gksquf6670@gmail.com>
2023-01-13 09:30:26 +0900

    naver

--- Node_200_Naver/routes/index.js
+++ Node_200_Naver/routes/index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import NAVER from "../config/naver_config.js";
+import {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  BOOK_URL_JSON,
+} from "../config/naver_config.js";
 const router = express.Router();
 
 /* GET home page. */
