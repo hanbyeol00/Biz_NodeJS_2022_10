d0ef2cfe4e489d6199301d92c416371a36bb7ae1
hanbyeol00 <gksquf6670@gmail.com>
2023-01-16 17:42:16 +0900

    naver

--- Node_200_Naver-V2/routes/book.js
+++ Node_200_Naver-V2/routes/book.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getBooks } from "../modules/naver_module.js";
+import Books from "../modules/books_module.js";
 import DB from "../models/index.js";
 // import { Op } from "sequelize";
 
@@ -18,7 +19,7 @@ router.get("/", async (req, res) => {
   /**
    * getBooks() 함수에서 throw new Error() 가 실행되면
    * getBooks() 함수를 호출하는 아래의 코드에서 Exception 이 발생한다
-   * 발생한 Exception 을 여기에서 해결한다
+   * 발생한 Exception 을 여기에서 처리(try catch)한다
    */
   try {
     const book_List = await getBooks(search);
@@ -38,8 +39,22 @@ router.get("/detail/:isbn", async (req, res) => {
 });
 
 router.post("/insert", async (req, res) => {
+  const user = {
+    username: "han",
+    password: "1234",
+    u_level: 0,
+    u_nickname: "길동",
+    u_name: "홍길동",
+  };
   const book = req.body;
-  // 도서정보를 books_modules.js 의 bookInput() 에게 이전하기
+
+  // 도서정보를 books_module.js 의 bookInput() 에게 이전하기
+  try {
+    await Books.bookInput(book, user);
+  } catch (e) {
+    console.log(e);
+    return res.json({ msg: "오류 발생", error: e });
+  }
 
   res.redirect("/book");
 });
--- Node_200_Naver-V2/routes/users.js
+++ Node_200_Naver-V2/routes/users.js
@@ -1,9 +1,44 @@
 import express from "express";
+import DB from "../models/index.js";
+const USER = DB.models.tbl_users;
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", async (req, res, next) => {
-  res.send("respond with a resource");
+router.get("/join", async (req, res, next) => {
+  const user = { username: "", password: "", re_password: "" };
+  return res.render("users/join", { ERROR: { CODE: 0 }, USER: user });
+});
+
+router.post("/join", async (req, res) => {
+  const { username, password, re_password } = req.body;
+  const resultError = { CODE: 0, MESSAGE: "" };
+
+  if (!username) {
+    resultError.CODE = 1;
+    resultError.MESSAGE = "* USERNAME 은 필수 항목입니다";
+    return res.render("users/join", { ERROR: resultError, USER: req.body });
+  }
+
+  if (!password) {
+    resultError.CODE = 2;
+    resultError.MESSAGE = "* 비밀번호를 반드시 입력해 주세요";
+    return res.render("users/join", { ERROR: resultError, USER: req.body });
+  }
+
+  if (!re_password) {
+    resultError.CODE = 3;
+    resultError.MESSAGE = "* 비밀번호 확인을 입력해 주세요";
+    return res.render("users/join", { ERROR: resultError, USER: req.body });
+  }
+
+  if (password !== re_password) {
+    resultError.CODE = 4;
+    resultError.MESSAGE = "* 비밀번호와 비밀번호 확인이 서로 다릅니다";
+    return res.render("users/join", { ERROR: resultError, USER: req.body });
+  }
+
+  /**
+   * 비밀번호 암호화
+   */
 });
 
 export default router;
