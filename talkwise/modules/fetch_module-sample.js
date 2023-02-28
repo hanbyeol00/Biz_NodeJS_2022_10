83ad5d20dc03a7c3c794978426e57f3f49ba2dd1
hanbyeol00 <gksquf6670@gmail.com>
2023-02-28 13:04:30 +0900

    talkwise

---
+++ talkwise/modules/fetch_module-sample.js
@@ -0,0 +1,17 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const getAnswering = async (question) => {
+  const configuration = new Configuration({
+    apiKey: "API KEY",
+  });
+  const openai = new OpenAIApi(configuration);
+  await new Promise((r) => setTimeout(r, 100));
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: question,
+    max_tokens: 1000,
+  });
+  return completion.data.choices[0].text;
+};
+
+export { getAnswering };
